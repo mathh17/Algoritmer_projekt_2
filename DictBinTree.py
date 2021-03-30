830069f7f7bd9dba01e827982dcac25c6aa3ac5b
Mathias <mathh17@student.sdu.dk>
2021-03-30 10:10:18 +0200

    Funktioner er indskrevet med sidetal

--- DictBinTree.py
+++ DictBinTree.py
@@ -2,5 +2,20 @@
 
 
 
+#Returnerer en boolean som angiver om nøglen k er i træet T (side 290-291)
+def search(T, k):
+    
+    
+#Indsæt nøglen k i træet T (side 294)
+def insert(T, k):
+    
+
+#Returnerer en liste med nøglerne i træet T i en sorteret orden (side 288) 
+def orderedTraversal(T):
+    
+    
+#Returnerer et nyt og tomt træ
+def createEmptyDict():
+
 
 
