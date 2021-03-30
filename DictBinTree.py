41d18a05bb03f652c00219e164db41cbe2f8e06d
Joachim Skovbogaard <Joachim.skovbogaard@gmail.com>
2021-03-30 11:51:45 +0200

    Auto stash before merge of "master" and "origin/master"

--- DictBinTree.py
+++ DictBinTree.py
@@ -4,10 +4,27 @@
 
 #Returnerer en boolean som angiver om nøglen k er i træet T (side 290-291)
 def search(T, k):
+    pass
     
     
 #Indsæt nøglen k i træet T (side 294)
-def insert(T, k):
+def insert(T, z):
+    y = None
+    x = root(T)
+    while x != None:
+        y = x
+        if key(z) < key(x):
+            x = left(x)
+        else:
+            x = right(x)        
+    if y == None:
+        T[0] = z     # Tree was empty, create root 
+    else if key(z) < key(y):
+        y[1] = z
+    else:
+        y[2] = z
+    
+
     
 
 #Returnerer en liste med nøglerne i træet T i en sorteret orden (side 288) 
@@ -25,4 +42,17 @@ def traverse_recursive(x):
 def createEmptyDict():
     return [None]
 
+
+
+def key(x):
+    return x[0]
+
+def left(x):
+    return x[1]
+
+def right(x):
+    return x[2]
+
+def root(x):
+    return x[0]
 
