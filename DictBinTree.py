eaac8d5d56b6a041665ab37d422d2317531d4845
Andreas <ankla20@student.sdu.dk>
2021-04-09 08:28:26 +0200

    change tree search

--- DictBinTree.py
+++ DictBinTree.py
@@ -3,23 +3,21 @@
 
 
 #Returnerer en boolean som angiver om nøglen k er i træet T (side 290-291)
-# def search(T, k):
-
-#virker ikke helt efter hensigten. 
-A = [15,6,18,3,7,17,20,2,4,13,9]
-
-#Returnerer index for den ønskede key
-def TREE_SEARCH(x,k):
-    if x == None or k == A[x]:
-        return x
-    if k < A[x]:
-        return TREE_SEARCH(x*2+1,k)
+# def search(T, k):     
+    if k == T[0]:
+        return True
+    if k < T[0]:
+        #tjekker on node i højre side er et blad
+        if T[1] == None:
+            return False
+        #forsætter søgning til venstre side i træet
+        return TREE_SEARCH(k,T[1])
     else:
-        return TREE_SEARCH(x*2+2,k)
-    
-    
-
-#print(TREE_SEARCH(0,7))
+        #tjekker on node i venstre side er et blad        
+        if T[2] == None:
+            return False        
+        #forsætter søgning til højre side i træet
+        return TREE_SEARCH(k,T[2])
     
 #Indsæt nøglen k i træet T (side 294)
 def insert(T, z):
